8b78f68ba5af0104556a5bac69d348111bd8dfab
stlavr <lawrentievsv@gmail.com>
2020-07-19 17:44:58 +0300

    Pull feature/multihash from kgusman/iroha-python

--- qa_utils/longivity/locust5TPS_MST.py
+++
@@ -1,7 +0,0 @@
-import locust_base
-
-class ApiUser(locust_base.IrohaLocust):
-    host = locust_base.random.choice(["s1.tst.iroha.tech:50051","s2.tst.iroha.tech:50051","s3.tst.iroha.tech:50051","s4.tst.iroha.tech:50051"])
-    min_wait = 1600
-    max_wait = 2400
-    task_set = locust_base.LivingNextDoorToMultiAlice
